db435e9773d51e6f3284dd7aa46e6cfac02c11d0
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-06-10 16:23:12 +0530

    Add Pattern13

--- pattern13.java
+++ pattern13.java
@@ -1,9 +1,11 @@
 class pattern13 {
   public static void main(String[] args) {
     int n = 5;
+    int num=1;
     for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= i; j++) {
-        System.out.print(j+1);
+        System.out.print(num+" ");
+        num=num+1;
       }
 
       System.out.println();
