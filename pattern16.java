9d201730f28523c5a434e7a7ed43f8bf10d48233
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-06-12 20:44:28 +0530

    Add pattern16

---
+++ pattern16.java
@@ -0,0 +1,12 @@
+public class pattern16 {
+  public static void main(String[] args) {
+    int n = 5;
+   for(int i=1;i<=n;i++){
+            for(char ch1='A';ch1<='A'+(n-i);ch1++){
+                System.out.print(ch1);
+            }
+            System.out.println();
+        }
+  }
+  
+}
