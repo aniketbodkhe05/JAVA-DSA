c79925e5d550c0330662aa8a288ddccf9ca9381d
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-07-01 09:39:19 +0530

    Add selectionsort program code

--- Sorting/Selectionsort.java
+++ Sorting/Selectionsort.java
@@ -6,6 +6,7 @@ public class Selectionsort {
     System.out.println("Enter the number of elements you want to sort: ");
     int n = sc.nextInt();
     int[] arr = new int[n];
+    System.out.println("Enter the elements: ");
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
     }
@@ -20,6 +21,12 @@ public class Selectionsort {
        int temp = arr[minIndex];
         arr[minIndex] = arr[i];
         arr[i] = temp;
+      }
+
+      System.out.println("Sorted array is: ");
+
+      for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i] + " ");
       }
   }
 
